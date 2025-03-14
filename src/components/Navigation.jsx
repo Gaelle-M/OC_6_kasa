25b32febeb9a46f236fffda445717c081f226f87
Gaelle-M <gaelle-mazzali@hotmail.fr>
2025-03-14 13:51:49 +0100

    Mise en place de la navigation et de la page d'accueil

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        <img src="./src/assets/img/logo-kasa.png" alt="Logo du site KASA" />
+        <ul>
+            <li>Accueil</li>
+            <li>À propos</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navigation
