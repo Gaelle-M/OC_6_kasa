e054fb103e96202061f87ca2bd1f87b9902ecd1c
Gaelle-M <gaelle-mazzali@hotmail.fr>
2025-03-14 13:35:01 +0100

    Mise en place du projet + mise en place des pages et components

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import de BrowserRouter
+import './index.scss';
+import App from './App';
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
