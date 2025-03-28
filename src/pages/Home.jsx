06c62474594668682c89fc0b35c00e5e601c0401
Gaelle-M <gaelle-mazzali@hotmail.fr>
2025-03-28 11:48:17 +0100

    ajout des cartes de la page d'accueil + reglage du style de la page d'accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import Navigation from '../components/Navigation'
-import Footer from '../components/footer'
+import React, { useState } from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import logementsData from "../data/data.json";
 
 const Home = () => {
+  const [logements] = useState(logementsData);
+
   return (
-    <div className='home'>
-        < Navigation />
-        <div className="container-title">
-          <h1>Chez vous, partout et ailleurs</h1>
-        </div>
+    <div className="home">
+      <Navigation />
+      <div className="container-title">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
 
-        <div className="container-card"></div>
-        < Footer />
+      <div className="container-cards">
+        {logements.map((logement) => (
+          <Card key={logement.id} title={logement.title} cover={logement.cover} />
+        ))}
+      </div>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
-  
+export default Home;
